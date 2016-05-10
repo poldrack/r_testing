a18da5e770aa000e35738996c3652657cd6e9064
poldrack <poldrack@gmail.com>
2016-05-10 12:48:43 -0700

    change to argument version

--- demean_age.R
+++ demean_age.R
@@ -1,7 +1,9 @@
-demographics <- read.csv(file='participants.tsv', head=TRUE, sep="\t")
+args=commandArgs(trailingOnly = TRUE)
+
+demographics <- read.csv(file=args[1], head=TRUE, sep="\t")
 age <- demographics[4]
 demean_age <- age - sum(age)/length(age)
 
-write.table(demean_ag, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
+write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
 
 print("done!")
