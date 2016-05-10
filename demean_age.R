ada0a58bb90242d4ff248a1996572b21e019df78
poldrack <poldrack@gmail.com>
2016-05-10 13:06:03 -0700

    fix data

--- demean_age.R
+++ demean_age.R
@@ -1,11 +1,13 @@
 args=commandArgs(trailingOnly = TRUE)
 
 demographics <- read.csv(file=args[1], head=TRUE, sep="\t")
-age <- demographics[4]
-demean_age <- age - sum(age)/length(age)
+age <- demographics$age
 mean_age=sum(age)/length(age)
+demean_age <- age - mean_age
 stopifnot(mean_age<100)
 stopifnot(mean_age>10)
 write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
 
 print("done!")
+
+
