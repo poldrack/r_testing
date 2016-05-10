2a7a6c7ba4e6c3745b73588e061b60e3ff17f19d
poldrack <poldrack@gmail.com>
2016-05-10 12:44:06 -0700

    breaking it

--- demean_age.R
+++ demean_age.R
@@ -2,6 +2,6 @@ demographics <- read.csv(file='participants.tsv', head=TRUE, sep="\t")
 age <- demographics[4]
 demean_age <- age - sum(age)/length(age)
 
-write.table(demean_age, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
+write.table(demean_ag, file="age_demeaned.tsv", row.names=FALSE, col.names=FALSE, sep="\t")
 
 print("done!")
